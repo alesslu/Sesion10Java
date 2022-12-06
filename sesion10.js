b4a4b69f99102d65e9170cf9d6c1492b2f6dce7c
alesslu <alessluchata@gmail.com>
2022-12-05 20:09:08 -0500

    añadiendo resolucion

---
+++ sesion10.js
@@ -0,0 +1,48 @@
+class Alumno {
+    constructor (codigo, nombre, nota1, nota2, nota3, nota4){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+    }
+    //Getter:promedio
+    get promedio(){
+        let prom = (this.nota1*0.15)+(this.nota2*0.2)+(this.nota3*0.25)+(this.nota4*0.4)
+        return "\nTu promedio es: "+ prom
+
+    }
+    //getter:condicion
+    get condicion(){
+        let prom = (this.nota1*0.15)+(this.nota2*0.2)+(this.nota3*0.25)+(this.nota4*0.4)
+        if (this.prom<12){
+            return "\nEstas desaprobado"
+        } else {
+            return "\nEstas aprobado"
+        }
+
+    }
+
+    //getter:obsequio
+    get obsequio(){
+        let prom = (this.nota1*0.15)+(this.nota2*0.2)+(this.nota3*0.25)+(this.nota4*0.4)
+        if (this.prom>17){
+            return "\nGanaste una mochila"
+        }else {
+            return "\nTe perdiste una mochila"
+        }
+
+
+    }
+}
+
+let id = prompt ("Id de alumno")
+let alum = prompt ("Ingresa Nombre")
+let not1 = prompt ("Nota 1:")
+let not2 = prompt ("Nota 2:")
+let not3 = prompt ("Nota 3:")
+let not4 = prompt ("Nota 4:")
+
+let alumnonew = new Alumno(id, alum, not1, not2, not3, not4)
+alert ("Hola "+alum+ alumnonew.promedio + alumnonew.condicion + alumnonew.obsequio)
